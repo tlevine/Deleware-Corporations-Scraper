96817e3c52332b30e033bd791b633ba278f0b2c4
Thomas Levine <thomas.levine@gmail.com>
2012-01-11 19:26:26 -0500

    aonsthuas

--- browser/browse.js
+++ browser/browse.js
@@ -1,3 +1,6 @@
+var URL='http://hacks.thomaslevine.com';
+var DIGGER_KEY='uoeusrch9898goun';
+/*
 //Define some search results page class
 //...
 //Done
@@ -12,4 +15,46 @@ p.send(function(){
   } else {
     //Try a new search with another letter before the wildcard.
   }
+}
+
+getResultsPage=function(searchterm,callback){
+
+}
+*/
+
+function main(){
+  $.ajax({
+    url: URL
+  , type: 'GET'
+  , data: JSON.stringify({
+      "digger-key":DIGGER_KEY
+    })
+  , dataType: 'json'
+  , contentType: "application/json"
+  , success: function(r){
+    var html=$('#mainBody').html();
+    $.ajax({
+      url: URL
+    , type: 'POST'
+    , data: JSON.stringify({
+        "searchterm":r.searchterm
+      , "html":html
+      , "shows_all"=shows_all()
+      })
+    , dataType: 'json'
+    , contentType: "application/json"
+    , success: function(r){
+        search(r.searchterm);
+      }
+    });
+  });
+}
+
+function shows_all(){
+  return 1===$('b:contains("Only the first 50 are displayed")').length;
+}
+
+function search(searchterm){
+  document.getElementsByName('frmEntityName')[0].value=searchterm;
+  $('input[name="action"]').click();
 }
